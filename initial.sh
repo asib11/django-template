42a5d6be11b84dcdd2074afc19730d9c3c0ca1d7
Asib Ahmed <ahmedasib1@gmail.com>
2026-04-01 12:35:57 +0600

    Update :  .env sample file reference in initial.sh

--- initial.sh
+++ initial.sh
@@ -2,7 +2,7 @@
 
 python3 -m venv .venv
 source .venv/bin/activate
-cp .env.sample .env
+cp .env.example .env
 pip install -r requirements.txt 
 python manage.py makemigrations
 python manage.py migrate
