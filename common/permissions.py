1383332a2a9b60bba86f516eec360d645b77df4a
asib11 <ahmedasib1@gmail.com>
2026-04-06 11:49:49 +0600

    feat : custom permission

--- common/permissions.py
+++ common/permissions.py
@@ -4,5 +4,9 @@ from rest_framework.permissions import *
 class IsUserVerified(IsAuthenticated):
     pass
 
-
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        return request.user and request.user.is_staff
 
