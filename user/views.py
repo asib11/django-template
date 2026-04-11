7ed865c1efbd354f451ea4c1141d6ff2dc9c88ec
asib11 <ahmedasib1@gmail.com>
2026-04-11 09:34:42 +0600

    fix : role set, welcome email

--- user/views.py
+++ user/views.py
@@ -20,6 +20,10 @@ from . import models as user_models
 from .permissions import IsAdminUser, IsActiveUser
 
 import random
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 
@@ -30,6 +34,32 @@ import random
 class UserRegisterAPIView(generics.CreateAPIView):
     permission_classes = []
     queryset = user_models.User.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+        instance = serializer.instance
+        user = instance.user
+
+        display_name = user.get_full_name().strip() or user.username or 'there'
+        html_message = render_to_string(
+            'welcome_email.html',
+            {
+                'user': user,
+                'project_name': env.PROJECT_NAME,
+                'year': timezone.now().year,
+            }
+        )
+
+        try:
+            send_mail(
+                subject=f'Welcome to {env.PROJECT_NAME}',
+                message=f'Hi {display_name}, welcome to {env.PROJECT_NAME}.',
+                from_email=env.EMAIL_HOST_USER,
+                recipient_list=[user.email],
+                html_message=html_message,
+            )
+        except Exception:
+            logger.exception('Failed to send welcome email for user_id=%s', user.pk)
 
 
 @create_view(
