30c469baca199f863cca2d2989279d944d776eeb
Asib Ahmed <ahmedasib1@gmail.com>
2026-07-22 09:02:32 +0600

    Modify entrypoint.sh for python3 and .env setup
    Updated entrypoint script to use python3 and added .env file check.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,30 @@
 
 
 ENV_FOLDER=".venv"
+ENV_FILE=".env"
 
 if [ ! -d "$ENV_FOLDER" ]; then
-    python -m venv $ENV_FOLDER
-    . $ENV_FOLDER/bin/activate
+    python3 -m venv $ENV_FOLDER
+    source $ENV_FOLDER/bin/activate
     pip install pip wheel setuptools -U
     pip install -r requirements.txt
 else
-    . $ENV_FOLDER/bin/activate
+    source $ENV_FOLDER/bin/activate
 fi
 
+if [ ! -f "$ENV_FILE" ]; then
+    echo "Copy .env.example to .env and configure it before running the application."
+    cp .env.example .env
+fi
 
+# mkdir -p staticfiles
+python3 manage.py collectstatic --no-input
 
-mkdir -p staticfiles
-python manage.py collectstatic --no-input
-
-python manage.py migrate
+python3 manage.py migrate
 
 celery -A projectile worker --loglevel=info --concurrency=4 --logfile=tmp/celery-worker.log &
 
-# python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
 # gunicorn projectile.wsgi \
 #     --access-logfile - \
@@ -29,4 +33,4 @@ celery -A projectile worker --loglevel=info --concurrency=4 --logfile=tmp/celery
 #     -b 0.0.0.0:8000
 
 # daphne -u /tmp/daphne.sock projectile.asgi:application
-daphne -b 0.0.0.0 -p 8000 projectile.asgi:application
+# daphne -b 0.0.0.0 -p 8000 projectile.asgi:application
