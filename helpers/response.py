37ed719a3e68d0c43bdf4b58637966ce484dbc39
Asib Ahmed <ahmedasib1@gmail.com>
2026-07-28 10:06:07 +0600

    feat: CustomResponseListMixin for paginated responses

--- helpers/response.py
+++ helpers/response.py
@@ -2,7 +2,27 @@ import re
 from rest_framework.response import Response
 from rest_framework import status
 
+class CustomResponseListMixin:
+    success_details = "Data fetched successfully."
+    success_code = "LIST_SUCCESS"
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = self.get_paginated_response(serializer.data).data
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            data = serializer.data
+
+        return response(
+            details=self.success_details,
+            code=self.success_code,
+            status_code=status.HTTP_200_OK,
+            data=data,
+        )
 
 def response(
         details: str,
